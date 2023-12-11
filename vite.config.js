df326bc740d89acfca634dc888704d49078ce79a
Lane <LeiLungSing@gmail.com>
2023-12-11 16:46:49 +0800

    測試gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    bases: "/vue-expense-tracker/",
+    bases: "./",
     plugins: [vue()],
     resolve: {
         alias: {
