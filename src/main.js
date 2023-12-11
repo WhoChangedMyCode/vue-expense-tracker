608592f34ee42c822dd6f1fce5fefc025d4ba478
Lane <LeiLungSing@gmail.com>
2023-12-11 16:17:25 +0800

    測試gh-pages

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 // vue-toastification是一個Vue的Toast通知庫。它可以用來在Vue應用程式中顯示簡短的信息消息。
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
-import "@/assets/style.css";
+import "./assets/style.css";
 import App from "./App.vue";
 
 const app = createApp(App);
